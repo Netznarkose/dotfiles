f1355674bfef1a6418b657f7b0bcc74f08c563d6
netznarkose <mmaintz@posteo.de>
2016-08-16 14:01:37 +0200

    improved git_loglive.sh log selections follows head now

--- git_loglive.sh
+++ git_loglive.sh
@@ -1,8 +1,15 @@
 #!
 
+number_of_logs=$1
+
+if [ -z "$number_of_logs" ]
+then
+    number_of_logs=40
+fi
+
 while :
 do
     clear
-    git --no-pager log --graph --pretty=oneline --abbrev-commit --decorate --all $*
+    git --no-pager log `git rev-parse HEAD` -n $number_of_logs --graph --pretty=oneline --abbrev-commit --decorate
     sleep 1
 done
