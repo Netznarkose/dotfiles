283cef521e47c82392b48fe631d27ac95850189f
netznarkose <mmaintz@posteo.de>
2017-02-05 17:31:57 +0100

    improve tmux_settings.sh

--- tmux_settings.sh
+++ tmux_settings.sh
@@ -1,6 +1,11 @@
 #!/bin/sh 
 tmux new-session -d 
 tmux split-window -h -p 40 
+tmux rename-window Git 
+tmux new-window
 tmux split-window -v -p 20
-tmux select-pane -t 0
+tmux rename-window Ruby 
+tmux select-window -t 0
+tmux select-pane -t 1
+
 tmux -2 attach-session -d 
