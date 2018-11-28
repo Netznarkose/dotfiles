3f617e33d3bdc1a651a17ee4136e972263e65de2
max <mmaintz@posteo.de>
2018-11-28 10:28:24 +0100

    set number of logs to 40 in git_loglive.sh

--- git_loglive.sh
+++ git_loglive.sh
@@ -2,7 +2,7 @@
  number_of_logs=$1
  if [ -z "$number_of_logs" ]
  then
-     number_of_logs=20 # set number of logs to 40 unless they are specified 
+     number_of_logs=40 # set number of logs to 40 unless they are specified 
  fi
  while :; do
    output=$(git --no-pager log -n $number_of_logs --graph --pretty=oneline --abbrev-commit --decorate --all --color=always)
